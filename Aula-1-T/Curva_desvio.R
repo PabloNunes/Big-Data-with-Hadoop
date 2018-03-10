342477de931bc574c2152affd06349879b041cd0
Pablo Nunes <pablonunes082@gmail.com>
2018-03-10 17:12:07 -0300

    R example

---
+++ Aula-1-T/Curva_desvio.R
@@ -0,0 +1,6 @@
+curve(dnorm(x,170,6),152,188,
+main="X~N(170,36)", ylab="probabilidade", xlab="altura")
+lines(c(182,182),c(0,0.06),col=2)
+
+# Distr. Normal -> Media = 170 e desv.padr = 6
+# Cor da linha vermelha
